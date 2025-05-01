c019e9a669d9da0a92ac0c797df004db2a598e15
Xuan Son Nguyen <son@huggingface.co>
2025-05-02 00:18:04 +0200

    add 2d-rope support for llama 4

--- demo/2d-rope.cpp
+++ demo/2d-rope.cpp
@@ -12,9 +12,10 @@
 static ggml_tensor * build_rope_2d(
     ggml_context * ctx0,
     ggml_tensor * cur,
-    ggml_tensor * pos_h,
-    ggml_tensor * pos_w,
-    const float freq_base
+    ggml_tensor * pos_a, // first half
+    ggml_tensor * pos_b, // second half
+    const float freq_base,
+    const bool interleave_freq
 ) {
     const int64_t n_dim  = cur->ne[0];
     const int64_t n_head = cur->ne[1];
@@ -28,10 +29,9 @@ static ggml_tensor * build_rope_2d(
     //  ^ don't ask me why, it's math! -2(2i) / n_dim == -2i / (n_dim/2)
     // then for the second half, we use freq_scale to shift the inv_freq
     //  ^ why? replace (2i) with (2i+1) in the above equation
-    const float freq_scale_odd = std::pow(freq_base, (float)-2/n_dim);
-    printf("freq_base = %f\n", freq_base);
-    printf("exp = %f\n", (float)-2/n_dim);
-    printf("freq_scale_odd = %f\n", freq_scale_odd);
+    const float freq_scale_odd = interleave_freq
+                                    ? std::pow(freq_base, (float)-2/n_dim)
+                                    : 1.0;
 
     // first half
     ggml_tensor * first;
@@ -44,7 +44,7 @@ static ggml_tensor * build_rope_2d(
         first = ggml_rope_ext(
             ctx0,
             first,
-            pos_h,      // positions
+            pos_a,      // positions
             nullptr,    // freq factors
             n_dim/2,    // n_dims
             0, 0, freq_base,
@@ -64,7 +64,7 @@ static ggml_tensor * build_rope_2d(
         second = ggml_rope_ext(
             ctx0,
             second,
-            pos_w,      // positions
+            pos_b,      // positions
             nullptr,    // freq factors
             n_dim/2,    // n_dims
             0, 0, freq_base,
@@ -81,9 +81,11 @@ int main() {
     ggml_easy::ctx_params params;
     ggml_easy::ctx ctx(params);
 
+    const bool is_llama = true; // false meaning pixtral
+
     const int n_sz  = 3;
-    const int n_pos = n_sz * n_sz;
-    const int n_dim = 12;
+    const int n_pos = n_sz * n_sz + (is_llama ? 1 : 0); // 1 for CLS token
+    const int n_dim = 8;
     const int n_head = 1;
 
     // create cgraph
@@ -92,21 +94,46 @@ int main() {
         ggml_tensor * pos_w = utils.new_input("pos_w", GGML_TYPE_I32, n_pos);
         ggml_tensor * vector = utils.new_input("vector", GGML_TYPE_F32, n_dim*n_head, n_pos);
         vector = ggml_reshape_3d(ctx_gf, vector, n_dim, n_head, n_pos);
-        ggml_tensor * result = build_rope_2d(ctx_gf, vector, pos_h, pos_w, 10000.0f);
+        ggml_tensor * result = is_llama
+            ? build_rope_2d(ctx_gf, vector, pos_w, pos_h, 10000.0f, false)
+            : build_rope_2d(ctx_gf, vector, pos_h, pos_w, 10000.0f, true);
         result = ggml_reshape_2d(ctx_gf, result, n_dim*n_head, n_pos);
         utils.mark_output(result, "result");
     });
 
     // set data
-    std::vector<int32_t> positions(n_pos);
-    for (int i = 0; i < n_pos; ++i) {
-        positions[i] = i / n_sz;
+    if (is_llama) {
+        auto py_div_floor = [](int a, int b) { // mimic "//" operator in python for negative numbers
+            return (a / b) - ((a % b != 0 && ((a < 0) ^ (b < 0))) ? 1 : 0);
+        };
+        auto py_mod = [](int a, int b) { // mimic "%" operator in python for negative numbers
+            return (a % b + b) % b;
+        };
+        std::vector<int32_t> positions(n_pos);
+        for (int i = 0; i < n_pos; ++i) {
+            positions[i] = (i / n_sz) + 1;
+            // printf("pos_h[%d] = %d\n", i, positions[i]);
+        }
+        positions[positions.size() - 1] = 0;
+        printf("\n");
+        ctx.set_tensor_data("pos_h", positions.data());
+        for (int i = 0; i < n_pos; ++i) {
+            positions[i] = (i % n_sz) + 1;
+            // printf("pos_w[%d] = %d\n", i, positions[i]);
+        }
+        positions[positions.size() - 1] = 0;
+        ctx.set_tensor_data("pos_w", positions.data());
+    } else {
+        std::vector<int32_t> positions(n_pos);
+        for (int i = 0; i < n_pos; ++i) {
+            positions[i] = i / n_sz;
+        }
+        ctx.set_tensor_data("pos_h", positions.data());
+        for (int i = 0; i < n_pos; ++i) {
+            positions[i] = i % n_sz;
+        }
+        ctx.set_tensor_data("pos_w", positions.data());
     }
-    ctx.set_tensor_data("pos_h", positions.data());
-    for (int i = 0; i < n_pos; ++i) {
-        positions[i] = i % n_sz;
-    }
-    ctx.set_tensor_data("pos_w", positions.data());
     ctx.set_tensor_data("vector", [](int i0, int i1, int i2, int i3) {
         //return i0 * 0.1;
         return 1.0;
