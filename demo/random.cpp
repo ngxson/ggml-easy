8ecd79156eb9e703983ef6b508e009cc9e281666
Xuan Son Nguyen <son@huggingface.co>
2025-06-01 13:21:22 +0200

    random 2

--- demo/random.cpp
+++ demo/random.cpp
@@ -87,5 +87,57 @@ int main() {
         ctx.compute();
     }
 
+    // https://github.com/ggml-org/llama.cpp/pull/13772
+    // {
+    //     const int h = 12;
+    //     const int w = 2;
+    //     const int hidden_size = 8;
+    //     ctx.build_graph([&](ggml_context * ctx0, ggml_cgraph * gf, auto & utils) {
+    //         ggml_tensor * inp = utils.new_input("inp", GGML_TYPE_F32, hidden_size, h*w);
+    //         inp = ggml_fill(ctx0, inp, 1.234f);
+    //         utils.debug_print(inp, "inp");
+    //     });
+    //     ctx.compute();
+    // }
+
+    // https://github.com/ggml-org/ggml/issues/1230
+    {
+        ggml_easy::ctx_params params_no_gpu;
+        params_no_gpu.use_gpu = false;
+        ggml_easy::ctx ctx_no_gpu(params_no_gpu);
+        ggml_easy::ctx ctx(params);
+        
+        auto builder = [&](ggml_context * ctx0, ggml_cgraph * gf, auto & utils) {
+            ggml_tensor * x = ggml_ones(ctx0, 512, 512);
+            x = ggml_scale(ctx0, x, 0.12432f);
+            ggml_tensor * y = ggml_ones(ctx0, 512, 512);
+            y = ggml_scale(ctx0, y, 0.34636f);
+
+            ggml_tensor * result = ggml_mul(ctx0, x, y);
+            utils.debug_print(result, "result");
+            utils.mark_output(result, "result");
+        };
+
+        ctx.build_graph(builder);
+        ctx.compute();
+        ctx_no_gpu.build_graph(builder);
+        ctx_no_gpu.compute();
+
+        float max_diff = 0.0f;
+        auto res0 = ctx.get_tensor_data("result");
+        auto res1 = ctx_no_gpu.get_tensor_data("result");
+        GGML_ASSERT(ggml_nelements(res0.first) == ggml_nelements(res1.first));
+        for (size_t i = 0; i < ggml_nelements(res0.first); ++i) {
+            float v0 = ((float *)res0.second.data())[i];
+            float v1 = ((float *)res1.second.data())[i];
+            float diff = std::abs(v0 - v1);
+            if (diff > max_diff) {
+                max_diff = diff;
+            }
+        }
+
+        printf("max diff: %f\n", max_diff);
+    }
+
     return 0;
 }
