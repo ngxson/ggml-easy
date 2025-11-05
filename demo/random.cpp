f0418d7555a5321f28d122ef28b2b48b8d659d35
Xuan Son Nguyen <son@huggingface.co>
2025-11-05 19:34:47 +0100

    demo minicpm-v pos embd sincos

--- demo/random.cpp
+++ demo/random.cpp
@@ -6,138 +6,192 @@
  * Random experiment, do not use it
  */
 
-int main() {
+
+
+
+
+ static std::vector<std::vector<std::vector<float>>> get_1d_sincos_pos_embed_from_grid_new(int embed_dim, const std::vector<std::vector<float>> & pos) {
+    GGML_ASSERT(embed_dim % 2 == 0);
+    int H = pos.size();
+    int W = pos[0].size();
+
+    std::vector<float> omega(embed_dim / 2);
+    for (int i = 0; i < embed_dim / 2; ++i) {
+        omega[i] = 1.0 / pow(10000.0, static_cast<float>(i) / (embed_dim / 2));
+    }
+
+    std::vector<std::vector<std::vector<float>>> emb(H, std::vector<std::vector<float>>(W, std::vector<float>(embed_dim)));
+    for (int h = 0; h < H; ++h) {
+        for (int w = 0; w < W; ++w) {
+            for (int d = 0; d < embed_dim / 2; ++d) {
+                float out_value = pos[h][w] * omega[d];
+                emb[h][w][d] = sin(out_value);
+                emb[h][w][d + embed_dim / 2] = cos(out_value);
+            }
+        }
+    }
+
+    return emb;
+}
+
+static std::vector<std::vector<std::vector<float>>> get_2d_sincos_pos_embed_from_grid(int embed_dim, const std::vector<std::vector<std::vector<float>>> & grid) {
+    GGML_ASSERT(embed_dim % 2 == 0);
+    std::vector<std::vector<std::vector<float>>> emb_h = get_1d_sincos_pos_embed_from_grid_new(embed_dim / 2, grid[0]); // (H, W, D/2)
+    std::vector<std::vector<std::vector<float>>> emb_w = get_1d_sincos_pos_embed_from_grid_new(embed_dim / 2, grid[1]); // (H, W, D/2)
+
+    int H = emb_h.size();
+    int W = emb_h[0].size();
+    std::vector<std::vector<std::vector<float>>> emb(H, std::vector<std::vector<float>>(W, std::vector<float>(embed_dim)));
+
+    for (int h = 0; h < H; ++h) {
+        for (int w = 0; w < W; ++w) {
+            for (int d = 0; d < embed_dim / 2; ++d) {
+                emb[h][w][d] = emb_h[h][w][d];
+                emb[h][w][d + embed_dim / 2] = emb_w[h][w][d];
+            }
+        }
+    }
+    return emb;
+}
+
+static std::vector<std::vector<float>> get_2d_sincos_pos_embed(int embed_dim, const std::pair<int, int> image_size) {
+    int grid_h_size = image_size.first;
+    int grid_w_size = image_size.second;
+
+    std::vector<float> grid_h(grid_h_size);
+    std::vector<float> grid_w(grid_w_size);
+
+    for (int i = 0; i < grid_h_size; ++i) {
+        grid_h[i] = static_cast<float>(i);
+    }
+    for (int i = 0; i < grid_w_size; ++i) {
+        grid_w[i] = static_cast<float>(i);
+    }
+
+    std::vector<std::vector<float>> grid(grid_h_size, std::vector<float>(grid_w_size));
+    for (int h = 0; h < grid_h_size; ++h) {
+        for (int w = 0; w < grid_w_size; ++w) {
+            grid[h][w] = grid_w[w];
+        }
+    }
+    std::vector<std::vector<std::vector<float>>> grid_2d = {grid, grid};
+    for (int h = 0; h < grid_h_size; ++h) {
+        for (int w = 0; w < grid_w_size; ++w) {
+            grid_2d[0][h][w] = grid_h[h];
+            grid_2d[1][h][w] = grid_w[w];
+        }
+    }
+
+    std::vector<std::vector<std::vector<float>>> pos_embed_3d = get_2d_sincos_pos_embed_from_grid(embed_dim, grid_2d);
+
+    int H = image_size.first;
+    int W = image_size.second;
+    std::vector<std::vector<float>> pos_embed_2d(H * W, std::vector<float>(embed_dim));
+    for (int h = 0; h < H; ++h) {
+        for (int w = 0; w < W; ++w) {
+            pos_embed_2d[w * H + h] = pos_embed_3d[h][w];
+        }
+    }
+
+    return pos_embed_2d;
+}
+
+
+
+
+int compare_minicpmv_pos_embd_ggml_cpp(int n_embd, int nx, int ny) {
     ggml_easy::ctx_params params;
     ggml_easy::ctx ctx(params);
 
-    // experiment with torch unfold equivalent in GGML
     {
-        const int h = 12;
-        const int w = 2;
-        const int hidden_size = 8;
         ctx.build_graph([&](ggml_context * ctx0, ggml_cgraph * gf, auto & utils) {
-            ggml_tensor * inp = utils.new_input("inp", GGML_TYPE_F32, hidden_size, h*w);
-            ggml_tensor * x = inp;
-            utils.debug_print(ggml_scale(ctx0, inp, 1.0f), "inp0");
+            GGML_ASSERT(n_embd % 4 == 0);
+            int n_pos = nx * ny;
+            ggml_tensor * pos_x = utils.new_input("pos_x", GGML_TYPE_F32, 1, n_pos);
+            ggml_tensor * pos_y = utils.new_input("pos_y", GGML_TYPE_F32, 1, n_pos);
+            ggml_tensor * omega = utils.new_input("omega", GGML_TYPE_F32, n_embd/4);
+            utils.debug_print(pos_x, "pos_x");
+            utils.debug_print(pos_y, "pos_y");
+            utils.debug_print(omega, "omega");
 
-            x = ggml_reshape_3d(ctx0, x, hidden_size, w, h);
-            x = ggml_permute(ctx0, x, 2, 0, 1, 3); // [x, y, hidden_size]
-            x = ggml_cont(ctx0, x);
-            utils.debug_print_full(x, "grid");
+            // outer product
+            ggml_tensor * omega_b = ggml_repeat_4d(ctx0, omega, n_embd/4, n_pos, 1, 1); // n_pos rows
+            ggml_tensor * theta_x = ggml_mul(ctx0, omega_b, pos_x);
+            ggml_tensor * theta_y = ggml_mul(ctx0, omega_b, pos_y);
 
-            ggml_tensor * kernel = ggml_view_3d(ctx0, inp, 2, 2, x->ne[2], 0, 0, 0);
-            x = ggml_im2col(ctx0, kernel, x, 2, 2, 0, 0, 1, 1, true, inp->type);
+            // sin and cos
+            ggml_tensor * pos_embd_x = ggml_concat(
+                ctx0,
+                ggml_sin(ctx0, theta_x),
+                ggml_cos(ctx0, theta_x),
+                0 // concat on first dim
+            );
+            ggml_tensor * pos_embd_y = ggml_concat(
+                ctx0,
+                ggml_sin(ctx0, theta_y),
+                ggml_cos(ctx0, theta_y),
+                0 // concat on first dim
+            );
 
-            utils.debug_print_full(x, "im2col");
+            ggml_tensor * pos_embd = ggml_concat(ctx0, pos_embd_x, pos_embd_y, 0);
+            utils.debug_print(pos_embd, "pos_embd");
 
-            x = ggml_reshape_2d(ctx0, x, x->ne[0], x->ne[1] * x->ne[2]);
-            utils.debug_print(x, "result");
-        });
-        std::vector<float> inp_data(h * w * hidden_size);
-        for (int i = 0; i < h * w * hidden_size; ++i) {
-            inp_data[i] = (float)i;
-        }
-        ctx.set_tensor_data("inp", inp_data.data());
-        ctx.compute();
-    }
+            // cpp version for comparison
+            ggml_tensor * cpp = utils.new_input("cpp", GGML_TYPE_F32, n_embd, nx*ny);
+            utils.debug_print(cpp, "cpp", true);
 
-    printf("\n\n\nLlama4UnfoldConvolution\n\n");
-    {
-        ggml_easy::ctx ctx(params);
-        ctx.load_safetensors("../models/llama4vit.safetensors", {});
-
-        ggml_tensor * patch_embeddings_0 = ctx.get_weight("vision_model.patch_embedding.linear.weight");
-
-        const int h = 336;
-        const int w = 336;
-        const int patch_size = 14;
-        const int n_embd = 1408;
-        const int n_patches = (h / patch_size) * (w / patch_size);
-
-        ctx.build_graph([&](ggml_context * ctx0, ggml_cgraph * gf, auto & utils) {
-            ggml_tensor * inp = utils.new_input("inp", GGML_TYPE_F32, h, w, 3);
-            
-            // Llama4UnfoldConvolution
-            {
-                ggml_tensor * kernel = ggml_reshape_4d(ctx0, patch_embeddings_0,
-                                                        patch_size, patch_size, 3, n_embd);
-                inp = ggml_im2col(ctx0, kernel, inp, patch_size, patch_size, 0, 0, 1, 1, true, inp->type);
-                //inp = ggml_reshape_2d(ctx0, inp, inp->ne[0], inp->ne[1] * inp->ne[2]); // flatten to 2D
-                utils.debug_print(inp, "im2col");
-                utils.debug_print(ggml_sum(ctx0, inp), "im2col_sum");
-
-                utils.debug_print(ggml_cast(ctx0, patch_embeddings_0, GGML_TYPE_F32), "patch_embeddings_0");
-
-                inp = ggml_mul_mat(ctx0, patch_embeddings_0, inp);
-                utils.debug_print(inp, "patch_conv");
-                utils.debug_print(ggml_sum(ctx0, inp), "patch_conv_sum");
-
-                inp = ggml_reshape_2d(ctx0, inp, n_embd, n_patches);
-            }
-
-            //inp = ggml_reshape_2d(ctx0, inp, inp->ne[0], inp->ne[1] * inp->ne[2]);
-            utils.debug_print(inp, "result");
+            ggml_tensor * x = ggml_sub(ctx0, pos_embd, cpp);
+            x = ggml_abs(ctx0, ggml_sum(ctx0, x));
+            utils.debug_print(x, "diff");
         });
 
-        std::vector<float> inp_data(h * w * 3, 0.0);
-        for (int i = 0; i < h * w; ++i) {
-            inp_data[i] = 1.0; //(float)i * 0.1;
+        // set input
+        std::vector<float> tmp(nx * ny);
+        for (int i = 0; i < nx * ny; ++i) {
+            tmp[i] = static_cast<float>(i % nx);
         }
-        ctx.set_tensor_data("inp", inp_data.data());
-        ctx.compute();
-    }
+        ctx.set_tensor_data("pos_x", tmp.data());
+        for (int i = 0; i < nx * ny; ++i) {
+            tmp[i] = static_cast<float>(i / nx);
+        }
+        ctx.set_tensor_data("pos_y", tmp.data());
+        tmp.resize(n_embd / 4);
+        const float base_freq = 10000.0f;
+        for (int i = 0; i < n_embd / 4; ++i) {
+            tmp[i] = 1.0f / std::pow(base_freq, static_cast<float>(i) / (n_embd / 4));
+        }
+        ctx.set_tensor_data("omega", tmp.data());
 
-    // https://github.com/ggml-org/llama.cpp/pull/13772
-    // {
-    //     const int h = 12;
-    //     const int w = 2;
-    //     const int hidden_size = 8;
-    //     ctx.build_graph([&](ggml_context * ctx0, ggml_cgraph * gf, auto & utils) {
-    //         ggml_tensor * inp = utils.new_input("inp", GGML_TYPE_F32, hidden_size, h*w);
-    //         inp = ggml_fill(ctx0, inp, 1.234f);
-    //         utils.debug_print(inp, "inp");
-    //     });
-    //     ctx.compute();
-    // }
-
-    // https://github.com/ggml-org/ggml/issues/1230
-    {
-        ggml_easy::ctx_params params_no_gpu;
-        params_no_gpu.use_gpu = false;
-        ggml_easy::ctx ctx_no_gpu(params_no_gpu);
-        ggml_easy::ctx ctx(params);
-        
-        auto builder = [&](ggml_context * ctx0, ggml_cgraph * gf, auto & utils) {
-            ggml_tensor * x = ggml_ones(ctx0, 512, 512);
-            x = ggml_scale(ctx0, x, 0.12432f);
-            ggml_tensor * y = ggml_ones(ctx0, 512, 512);
-            y = ggml_scale(ctx0, y, 0.34636f);
-
-            ggml_tensor * result = ggml_mul(ctx0, x, y);
-            utils.debug_print(result, "result");
-            utils.mark_output(result, "result");
-        };
-
-        ctx.build_graph(builder);
-        ctx.compute();
-        ctx_no_gpu.build_graph(builder);
-        ctx_no_gpu.compute();
-
-        float max_diff = 0.0f;
-        auto res0 = ctx.get_tensor_data("result");
-        auto res1 = ctx_no_gpu.get_tensor_data("result");
-        GGML_ASSERT(ggml_nelements(res0.first) == ggml_nelements(res1.first));
-        for (size_t i = 0; i < ggml_nelements(res0.first); ++i) {
-            float v0 = ((float *)res0.second.data())[i];
-            float v1 = ((float *)res1.second.data())[i];
-            float diff = std::abs(v0 - v1);
-            if (diff > max_diff) {
-                max_diff = diff;
+        // cpp version for comparison
+        auto pos_embed_t = get_2d_sincos_pos_embed(n_embd, std::make_pair(nx, ny));
+        std::vector<float> pos_embed(nx * ny * n_embd);
+        for(int i = 0; i < nx * ny; ++i){
+            for(int j = 0; j < n_embd; ++j){
+                pos_embed[i * n_embd + j] = pos_embed_t[i][j];
             }
         }
+        ctx.set_tensor_data("cpp", pos_embed.data());
 
-        printf("max diff: %f\n", max_diff);
+
+        ctx.compute();
+
+        auto result = ctx.get_tensor_data("diff");
+        ggml_tensor * result_tensor        = result.first;
+        std::vector<uint8_t> & result_data = result.second;
+        float * result_data_f32 = (float *) result_data.data();
+        float diff = result_data_f32[0];
+        std::cout << "diff = " << diff << std::endl;
+        GGML_ASSERT(diff < 1e-4);
     }
 
+    return 0;
+}
+
+int main() {
+    compare_minicpmv_pos_embd_ggml_cpp(8, 3, 2);
+    compare_minicpmv_pos_embd_ggml_cpp(16, 5, 4);
+    compare_minicpmv_pos_embd_ggml_cpp(64, 4, 7);
+    compare_minicpmv_pos_embd_ggml_cpp(128, 8, 16);
+    std::cout << "all tests passed" << std::endl;
     return 0;
 }
